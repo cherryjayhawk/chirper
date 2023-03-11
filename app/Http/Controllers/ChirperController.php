4341484b0155a20cf0b530e8ae20730decda1f51
cherryjayhawk <muhbintangr19@gmail.com>
2023-03-11 20:00:22 +0700

    create chirpers models, migrations, controllers

---
+++ app/Http/Controllers/ChirperController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirper;
+use Illuminate\Http\Request;
+
+class ChirperController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Chirper  $chirper
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Chirper $chirper)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Chirper  $chirper
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Chirper $chirper)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Chirper  $chirper
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Chirper $chirper)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Chirper  $chirper
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chirper $chirper)
+    {
+        //
+    }
+}
